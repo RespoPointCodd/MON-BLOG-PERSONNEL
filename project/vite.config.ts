1c867712a340d15f11c2c0f813c0da254b1ed13e
RespoPointCodd <respopointcodd@gmail.com>
2025-05-24 02:27:20 +0200

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MON-BLOG-PERSONNEL/', // ← très important pour GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
